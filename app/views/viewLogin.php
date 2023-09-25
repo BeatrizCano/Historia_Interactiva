34079604a1601bc7e9acd392717d97b64d4796ee
BeatrizCano <beatrizcafernandez@gmail.com>
2023-09-25 15:14:52 +0200

    add_styles_login

--- app/views/viewLogin.php
+++ app/views/viewLogin.php
@@ -30,25 +30,68 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     include ("../../templates/head.php");
 ?>
 
-    <h1>Iniciar Sesión</h1>
-    
-    <form class="" action="viewLogin.php" method="POST">
-        <div class="alert <?php echo (!empty($error_message)) ? 'alert-danger' : 'd-none'; ?>" role="alert">
-            <?php if (!empty($error_message)) { ?>
-                <p><?php echo $error_message; ?></p>
-            <?php } ?>
+<link rel="stylesheet" href="../../public/css/style.css">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 d-flex align-items-center justify-content-center custom-div">
+            <div class="card custom-font mt-3 border-0" style="background-color: #F5F5DC;">
+                <img src="../../assets/img/wallpapers/photo-book-1318702_1920.png" class="img-fluid custom-image" style="height: 80vh;" alt="...">
+            </div>
         </div>
-            <div>
-            <label for="name">Nombre de usuario:</label>
-            <input type="text" name="name" required>
-            <br>
-            <label for="password">Contraseña:</label>
-            <input type="password" name="password" required>
-            <br>
-            <input type="submit" value="Iniciar Sesión">
-        </div>       
-    </form>
-    <p>¿Aún no tienes una cuenta? <a href="./viewRegister.php">Registrarse</a></p>
+        <div class="col-md-6 d-flex align-items-center justify-content-center">
+    <div class="card custom-font border-0" style="background-color: #F5F5DC;">
+        <div class="card-body text-center">
+
+            <h1 class="mb-5 custom-font card-title">Iniciar Sesión</h1>
+
+            <div class="col-md-12 mx-auto">
+
+            <form action="viewLogin.php" method="POST">
+                <div class="alert <?php echo (!empty($error_message)) ? 'alert-danger' : 'd-none'; ?>" role="alert">
+                    <?php if (!empty($error_message)) { ?>
+                        <p><?php echo $error_message; ?></p>
+                    <?php } ?>
+                </div>
+
+                <div class="mb-3 text-start row align-items-center">
+                    <div class="col-md-3">
+                        <label for="name" class="form-label d-inline-block mb-1">Usuario:</label>
+                    </div>
+                    <div class="col-md-9">
+                        <input type="text" class="form-control d-inline-block" id="name" name="name" required>
+                    </div>
+                </div>
+
+
+                <div class="mb-3 text-start row align-items-center">
+                    <div class="col-md-4">
+                        <label for="password" class="form-label d-inline-block mb-1">Contraseña:</label>
+                    </div>
+                    <div class="col-md-8">
+                        <input type="password" class="form-control d-inline-block" name="password" id="password" required>  
+                    </div>                    
+                </div>
+
+                <div class="mb-3 form-check text-start">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Recuérdame</label>
+                </div>
+
+                <div class="d-grid">
+                    <input type="submit" class="btn btn-primary" value="Iniciar Sesión">
+                </div>
+            </form>
+        </div>
+
+            <p class="mt-3">¿Aún no tienes una cuenta? <a href="./viewRegister.php">Registrarse</a></p>
+
+        </div>
+    </div>
+</div>
+
+    </div>
+</div>
+
 
     <?php
         include ("../../templates/footer.php");
