789f8b2c106d33ae8ff3016994b84107ee5e81a7
BeatrizCano <beatrizcafernandez@gmail.com>
2023-09-25 10:57:47 +0200

    change_html_css_index

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500&family=Mulish:wght@200;300;400&display=swap" rel="stylesheet">
 
 </head>
-<body class="d-flex flex-column min-vh-100 ">
+<body class="d-flex flex-column min-vh-100" style="background-color: #F5F5DC;">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">
@@ -40,25 +40,29 @@
     </div>
     </nav>
 
-    <div class="card mb-3 custom-font" style="background-color: #F5F5DC;">
-    <div class="row g-0">
-        <div class="col-md-4 d-flex align-items-center justify-content-center">
-            <img src="./assets/img/wallpapers/photo-book-1318702_1920.png" class="img-fluid rounded-start" alt="...">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-6 d-flex align-items-center justify-content-center custom-div">
+            <div class="card custom-font mt-3 border-0" style="background-color: #F5F5DC;">
+                <img src="./assets/img/wallpapers/photo-book-1318702_1920.png" class="img-fluid custom-image" style="height: 80vh;" alt="...">
+            </div>
         </div>
-        <div class="col-md-8 d-flex align-items-center"> <!-- Agregar las clases d-flex y align-items-center aquí -->
-            <div class="card-body text-center">
-                <h1 class="card-title pb-3 pt-3">Bienvenido a Historia Interactiva</h1>
-                <p class="card-text">Aquí podrás explorar emocionantes historias personalizadas, en las que tú serás el protagonista.</p>
-                <p class="card-text">Hoy te ofrecemos un cuento clásico, que trata de un reino gobernado por l@s gat@s, con la ayuda de una Inteligencia Artificial 
+        <div class="col-md-6 d-flex align-items-center justify-content-center">
+            <div class="card custom-font border-0" style="background-color: #F5F5DC;">
+                <div class="card-body text-center">
+                    <h1 class="card-title">Bienvenido a Historia Interactiva</h1>                    
+                    <p class="card-text">Aquí podrás explorar emocionantes historias personalizadas, en las que tú serás el protagonista.</p>
+                    <p class="card-text">Hoy te ofrecemos un cuento clásico, que trata de un reino gobernado por l@s gat@s, con la ayuda de una Inteligencia Artificial 
                     que dirige a un ejército de robots. Sus aliados son l@s elefant@s, que habitan en un reino próximo. Y sus enemigos naturales son l@s raton@s, a quienes pertenecían las tierras 
                     en la antigüedad. Nuestro protagonista es un@ pequeñ@ elefant@ recién llegado al reino de l@s gat@s. Las decisiones que tome, decidirán el futuro
                     del mundo que conocen, para bien o para mal. ¿Quieres ayudarle a decidir sabiamente? Su futuro está ahora en tus manos.</p>
-                <p class="card-text">Por favor, regístrate o inicia sesión para comenzar la aventura..¡Vamos a ello!</p>
-            
+                    <p class="card-text">Por favor, regístrate o inicia sesión para comenzar la aventura..¡Vamos a ello!</p>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
     <?php
         include ("./templates/footer.php");
     ?>
