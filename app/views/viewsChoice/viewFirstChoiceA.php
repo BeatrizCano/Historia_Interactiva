31eccf923c8d5e965aebcc6767c696477bc01c7f
BeatrizCano <beatrizcafernandez@gmail.com>
2023-11-19 14:04:34 +0100

    add_styles_history_wiews

--- app/views/viewsChoice/viewFirstChoiceA.php
+++ app/views/viewsChoice/viewFirstChoiceA.php
@@ -10,44 +10,79 @@ include ("../../../sections/session/sessionStart.php");
     include ("../../../templates/headViewsChoice.php");
 ?>
 
-    <?php
-    // Incluir tus funciones y archivos necesarios aquí
+<body>
 
-    // Obtener la historia 2 de la tabla historias
-    $connection = createConnection("interactive_history");
-    $storyId = 2; // Cambia esto por el ID de la historia que deseas obtener
+<div class="card bg-dark text-white">
+    <img src="../../../assets/img/ilustracion-de-dibujos-animados-de-elefante-bebe.jpg" class="card-img" alt="...">
+    <div class="card-img-overlay">
+        <div class="custom-font border-0 login-container-history text-container">
+            <div class="card-body text-center">
+                <div id="carouselExample" class="carousel slide" data-bs-interval="false">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
 
-     //Para incluir todas las consultas a las tablas y la lógica
-     include ("../../../sections/querys/queryViewHistoryChoice.php");
+                            <?php
+                                // Incluir tus funciones y archivos necesarios aquí
 
-     // Mostrar el título y contenido de la primera historia
-     echo "<h1>$storyTitle</h1>";
-     echo "<p>$storyContent</p>";
+                                // Obtener la historia 2 de la tabla historias
+                                $connection = createConnection("interactive_history");
+                                $storyId = 2; // Cambia esto por el ID de la historia que deseas obtener
 
-    ?>     
+                                //Para incluir todas las consultas a las tablas y la lógica
+                                include ("../../../sections/querys/queryViewHistoryChoice.php");
 
-<form action="viewHistory.php" method="POST">
-    <p>Elige una de las siguientes opciones:</p>
-    <?php
-    // Mostrar las opciones disponibles en forma de texto
-    while ($optionRow = mysqli_fetch_assoc($optionsResult)) {
-        $optionId = $optionRow['id_opcion'];
-        $optionText = $optionRow['texto'];
-        echo "<p><span style='margin-right: 5px;'>•</span>$optionText</p>";
-    }
-    ?>
-</form>
-<br> <!-- Agrega un salto de línea para separar las dos opciones -->
-<div>
-    <form action="viewASecondChoiceA.php" method="POST" style="display: inline-block; margin-right: 10px;">
-        <input type="hidden" name="chosen_option" value="1">
-        <button type="submit">Ir a Opción A</button>
-    </form>
-    <form action="viewASecondChoiceB.php" method="POST" style="display: inline-block;">
-        <input type="hidden" name="chosen_option" value="2">
-        <button type="submit">Ir a Opción B</button>
-    </form>
+                                // Mostrar el título y contenido de la primera historia
+                                echo "<h1>$storyTitle</h1>";
+                                echo "<p>$storyContent</p>";
+
+                            ?>     
+
+                        </div>
+
+                        <div class="carousel-item">
+
+                            <form action="viewHistory.php" method="POST">
+                                <p>Elige una de las siguientes opciones:</p>
+                                <?php
+                                // Mostrar las opciones disponibles en forma de texto
+                                while ($optionRow = mysqli_fetch_assoc($optionsResult)) {
+                                    $optionId = $optionRow['id_opcion'];
+                                    $optionText = $optionRow['texto'];
+                                    echo "<p><span style='margin-right: 5px;'>•</span>$optionText</p>";
+                                }
+                                ?>
+                            </form>
+                            <br> <!-- Agrega un salto de línea para separar las dos opciones -->
+
+                        <div>
+                            <form action="viewASecondChoiceA.php" method="POST" style="display: inline-block; margin-right: 10px;">
+                                <input type="hidden" name="chosen_option" value="1">
+                                <button type="submit">Ir a Opción A</button>
+                            </form>
+                            <form action="viewASecondChoiceB.php" method="POST" style="display: inline-block;">
+                                <input type="hidden" name="chosen_option" value="2">
+                                <button type="submit">Ir a Opción B</button>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev" >
+                    <span class="carousel-control-prev-icon" aria-hidden="true" style="background-color: black"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true" style="background-color: black"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
 </div>
+   
+
 <?php
     include ("../../../templates/footer.php");
 ?> 
+    
+
