b61ea3312845dd22109e1efb48b3257458bc5ee7
BeatrizCano <beatrizcafernandez@gmail.com>
2023-12-25 19:43:29 +0100

    change_font_type

--- templates/headViewsChoice.php
+++ templates/headViewsChoice.php
@@ -16,7 +16,7 @@
     <!--Fuente de letra-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500&family=Mulish:wght@200;300;400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
