7ca2557b82ac736cb624fc3537884387c0700171
Beatriz Cano <130498392+BeatrizCano@users.noreply.github.com>
2024-01-03 15:06:23 +0100

    Update headViewsFinals.php

--- templates/headViewsFinals.php
+++ templates/headViewsFinals.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/css/styles.css">
-    <link rel="stylesheet" href="../../../../public/css/style8.css">
+    <link rel="stylesheet" href="../../../../public/css/styles8.css">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
