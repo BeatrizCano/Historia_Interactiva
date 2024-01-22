718a3c8d1b98296db2dd2a5140db0e75b4f2323f
Beatriz Cano <130498392+BeatrizCano@users.noreply.github.com>
2024-01-22 13:41:00 +0100

    remove_folder_styles

--- templates/head.php
+++ templates/head.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Historia Interactiva</title>
-    <link rel="stylesheet" href="../../public/css/styles.css">
     <link rel="stylesheet" href="../../public/css/style9.css">
     
 </head>
